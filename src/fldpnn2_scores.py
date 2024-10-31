b2fef8fe7ce496396b67ec7cebfa2e686e7a7f3a
gdziubcio <g.olszak@student.uw.edu.pl>
2024-10-31 12:03:36 +0100

    Function for extracting data from fldpnn2 text file

---
+++ src/fldpnn2_scores.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+def get_scores(path: str = "data/fldpnn2.txt") -> pd.DataFrame:
+    """
+    Reads a txt file from fldpnn2 containing disorder sequences, 
+    processes the data, and returns a DataFrame.
+
+    Args:
+        path (str, optional): The file path to the disorder sequences data.
+                              Defaults to "data/fldpnn2.txt".
+
+    Returns:
+        pd.DataFrame: A DataFrame containing two columns:
+                      - "ID": The cleaned sequence identifiers.
+                      - "fldpnn2_score": The corresponding disorder scores.
+    """
+    # Initialize an empty dictionary to store sequence IDs and their scores
+    fldpnn2_dict: dict = {}
+
+    with open(path, mode="r") as file:
+
+        lines: list = file.readlines()
+        
+        # header occupies always first 8 lines
+        cleaned_lines = [line.strip() for line in lines[8:]]
+        
+        # Extract sequence IDs and disorder scores:
+        # - Sequence IDs are assumed to be every 5th line starting from the first
+        # - Disorder scores are assumed to be every 5th line starting from the fifth
+        sequence_ids = cleaned_lines[0::5]
+        disorder_scores = cleaned_lines[4::5]
+        
+        # Create a dictionary mapping each sequence ID to its disorder score
+        fldpnn2_dict = {
+            name: disorder_seq 
+            for name, disorder_seq in zip(sequence_ids, disorder_scores)
+        }
+
+
+    df_fldpnn2 = pd.DataFrame(
+        fldpnn2_dict.items(), 
+        columns=["ID", "fldpnn2_score"]
+    )
+    
+    # clean the sequence identifiers
+    df_fldpnn2["ID"] = df_fldpnn2["ID"].str.replace(">", "", regex=False)
+    
+    return df_fldpnn2
